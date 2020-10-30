d6e5a00e4302bb4b50ff22c03c649d96419d71cd
AlexeyVolchenkov <altay197@yandex.ru>
2020-10-30 10:00:35 +0300

    Вывод чередования 0 и 1, начиная с 0, но на индексе 4 стоит 3 и на индексе 5 стоит 2, всего 10 символов

--- part2.cpp
+++ part2.cpp
@@ -4,11 +4,17 @@ int main()
 {
 	int numbers[10], x, z = 0;
 	for (x = 0; x <= 9; x++) {
-		if (x % 2 == 0) {
+		if (x % 2 == 0 && x != 4 && x != 5) {
 			numbers[x] = 0;
 		}
 		else {
 			numbers[x] = 1;
+		}
+		if (x == 4) {
+			numbers[x] = 3;
+		}
+		if (x == 5) {
+			numbers[x] = 2;
 		}
 	}
 	while (z <= 9) {
